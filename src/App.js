dbacb35487e8fd2b60bc254d37ccdff4fc25c371
NHS-Reed-03 <107868034+NHS-Reed-03@users.noreply.github.com>
2025-05-10 11:07:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with CI/CD (@sonnh-21IT)
+          Learn React with CI/CD sonnh-21IT
         </a>
       </header>
     </div>
